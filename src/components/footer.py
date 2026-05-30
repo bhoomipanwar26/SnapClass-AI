7f1d85dfed593f7a278709787a4080bc80e13515
Bhoomi <bhoomi.panwar26@gmail.com>
2026-05-31 01:28:13 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+def footer_home():
+
+    st.markdown("""
+        <div style="margin-top:2rem; display:flex; justify-content:center;">
+            <p style="font-weight:bold; color:white;">
+                Developed by Bhoomi Panwar
+            </p>
+        </div>
+    """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+
+    st.markdown("""
+        <div style="margin-top:2rem; display:flex; justify-content:center;">
+            <p style="font-weight:bold; color:black;">
+                Developed by Bhoomi Panwar
+            </p>
+        </div>
+    """, unsafe_allow_html=True)
