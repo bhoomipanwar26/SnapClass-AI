575b135d9015e139eb1866ba8a1355dd26fd6b2f
Bhoomi <bhoomi.panwar26@gmail.com>
2026-06-01 19:42:08 +0530

    URL update

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -6,7 +6,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "snapclass-main.streamlit.app"
+    app_domain = "bhoomipanwar26-snapclass-ai-app-nbfga4.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
